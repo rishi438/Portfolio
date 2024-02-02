8c5e7f4213c256612ddaa0a774037edffbac0338
xenn <rishi734438@gmail.com>
2024-02-02 19:58:41 +0530

    main setup

---
+++ portfolio/src/components/Loader.jsx
@@ -0,0 +1,16 @@
+// @ts-nocheck
+import { Html } from '@react-three/drei';
+const Loader = () => {
+  return (
+    <Html>
+      <div className="flex justify-center items-center">
+        <div
+          className="w-20 h-20 border-2 border-opacity-20
+        border-red-500 border-t-red-500 rounded-full animate-spin"
+        />
+      </div>
+    </Html>
+  );
+};
+
+export default Loader;
