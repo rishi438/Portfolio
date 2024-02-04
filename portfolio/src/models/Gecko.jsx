855aea02a5e409615b77b32ff9536aaf66317598
xenn <rishi734438@gmail.com>
2024-02-04 23:28:42 +0530

    small ui fixes v2

--- portfolio/src/models/Gecko.jsx
+++ portfolio/src/models/Gecko.jsx
@@ -1,13 +1,29 @@
 // @ts-nocheck
 import { useEffect, useRef } from "react";
 import { useAnimations, useGLTF } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
 import geckoScene from "../assets/3d/gecko.glb";
 
 const Gecko = ({ current_anime, set_current_anime, ...props }) => {
   const gecko_ref = useRef();
   const { scene, animations } = useGLTF(geckoScene);
   const { actions } = useAnimations(animations, gecko_ref);
-
+  const { gl } = useThree();
+  const handle_gecko = async (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    if (current_anime == "FG_Idle_A") {
+      set_current_anime("CH_Fallguy_FG_Emote_Mexwave");
+      await setTimeout(() => set_current_anime("FG_Idle_A"), 2000);
+    }
+  };
+  useEffect(() => {
+    const canvas = gl.domElement;
+    canvas.addEventListener("click", handle_gecko);
+    return () => {
+      canvas.removeEventListener("dblclick", handle_gecko);
+    };
+  }, [gl]);
   let is_initial = useRef(true);
   useEffect(() => {
     Object.values(actions).forEach((action) =>
